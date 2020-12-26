fb81a3600f95840953add97de5b88ceb6d28df3e
RAJAT MEHRA <35677839+ryanjonas05@users.noreply.github.com>
2020-12-26 21:13:18 +0530

    before creating backend

---
+++ src/components/Transaction.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from './context/GlobalState'
+
+export default function Transaction({transaction}) {
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    //delete
+    const {deleteTransaction} = useContext(GlobalContext)
+
+    return (
+        <div>
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+                    {transaction.text} <span> {sign}Rs. {Math.abs(transaction.amount)}</span> <button onClick={()=> deleteTransaction(transaction.id)} className="delete-btn"> X </button>
+                </li>
+        </div>
+    )
+}
