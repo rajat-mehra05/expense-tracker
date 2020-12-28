4d05b9eaa2045f7a268edd92ba65996812c3c1e6
RAJAT MEHRA <35677839+ryanjonas05@users.noreply.github.com>
2020-12-28 14:23:48 +0530

    added some change in contact me

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,7 +3,7 @@ import {Card, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const getStyle = {
-    paddingLeft: '3px',
+    paddingLeft: '290px',
     textDecoration: 'underline',
     fontSize: "1.4rem",
     fontWeight: '700',
@@ -13,16 +13,18 @@ const getStyle = {
 export default function Contact() {
     return (
         <>
-        <Link to ="/" style={getStyle}> ← Back to Home</Link> <br />
-        <div className="col d-flex justify-content-center my-5">
-        <Card style={{ width: '25rem' }}>
-  <Card.Img variant="top" src="https://avatars2.githubusercontent.com/u/35677839?s=460&u=a104c25a68766afb02f654b513544379fe729925&v=4" height="400px"/>
+        <Link to ="/" style={getStyle}> ← Back to Home</Link>
+        <div>
+        <Card style={{ width: '20rem' }}>
+
+  <Card.Img variant="top" src="https://avatars2.githubusercontent.com/u/35677839?s=460&u=a104c25a68766afb02f654b513544379fe729925&v=4" height="230px" />
+
   <Card.Body>
     <Card.Title> Rajat Mehra</Card.Title>
     <Card.Text>
       I'm a Web Developer. I build web apps and produce aesthetic websites.
     </Card.Text>
-    <Button variant="primary" href="https://www.linkedin.com/in/rajat-mehra-/"> LinkedIn </Button> {' '}
+     <Button variant="primary" href="https://www.linkedin.com/in/rajat-mehra"> LinkedIn </Button>
     <Button variant="primary" href="https://github.com/rajat-mehra05"> Github </Button>
   </Card.Body>
 </Card>
